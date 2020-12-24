a4e862fa7c259e81cbc1882c34df3ebe0474b16f
juliansyahrifqi <rifqipratama9@gmail.com>
2020-12-24 15:07:23 +0700

    add edit and update process

--- app.js
+++ app.js
@@ -68,6 +68,29 @@ app.post('/delete/:id', (req, res) => {
 
         res.redirect('/');
     });
-    
+});
+
+app.get('/edit/:id', (req, res) => {
+    const sql = "SELECT * FROM Users WHERE user_id = $1";
+
+    pool.query(sql, [req.params.id], (err, result) => {
+        if(err) {
+            return console.log(err.message);
+        }
+
+        res.render('edit.ejs', {user: result.rows[0]});
+    });
+});
+
+app.post('/update/:id', (req, res) => {
+    const sql = "UPDATE Users SET nama_user = $1 WHERE user_id = $2";
+
+    pool.query(sql, [req.body.nama, req.params.id], (err, result) => {
+        if(err) {
+            return console.log(err.message);
+        }
+
+        res.redirect('/');
+    });
 });
 
