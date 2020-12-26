652873ff9213e6304dae64800e9db4d4912af61f
juliansyahrifqi <rifqipratama9@gmail.com>
2020-12-26 16:21:09 +0700

    create script.js in public folder

---
+++ public/js/script.js
@@ -0,0 +1,33 @@
+const checkbox = document.getElementById('check');
+const currentTheme = localStorage.getItem("theme");
+const btn = document.querySelector(".btn");
+
+// Dark mode script
+if(currentTheme == "dark") {
+    document.body.classList.toggle("dark-theme");
+    checkbox.checked = true;
+}
+
+check.addEventListener('change', () => {
+    document.body.classList.toggle("dark-theme"); 
+
+    let theme = "light";
+    
+    if(document.body.classList.contains("dark-theme")) {
+        theme = "dark";
+    }
+
+    localStorage.setItem("theme", theme);
+});
+
+// Add form validation
+function validateForm() {
+    const addForm = document.forms["addForm"]["nama"].value;
+
+    if(addForm == "") {
+        alert("Nama tidak boleh kosong!");
+        return false;
+    }
+}
+
+
